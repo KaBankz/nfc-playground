968555805ff570872342837ee7afffd3d4d07308
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-12-02 17:24:07 -0500

    Show header from _layout

--- src/app/(tabs)/write.tsx
+++ src/app/(tabs)/write.tsx
@@ -3,7 +3,6 @@ import { Alert, ScrollView, TextInput, View } from 'react-native';
 
 import { t } from '@lingui/core/macro';
 import { Trans } from '@lingui/react/macro';
-import { Tabs } from 'expo-router';
 import NfcManager, { Ndef, NfcTech } from 'react-native-nfc-manager';
 
 import { Pressable } from '@/components/Pressable';
@@ -29,47 +28,44 @@ export default function WriteTag() {
   }
 
   return (
-    <>
-      <Tabs.Screen options={{ headerShown: true }} />
-      <ScrollView className='flex-1 p-6' contentContainerClassName='gap-4'>
-        <View className='flex-row gap-4'>
-          <Pressable
-            className='grow items-center rounded-lg bg-neutral-800 px-4 py-6 transition-colors active:bg-neutral-700'
-            onPress={writeNdef}>
-            <Text className='text-xl font-semibold'>
-              <Trans>Write NFC Tag</Trans>
-            </Text>
-          </Pressable>
-          <Pressable
-            className='grow items-center rounded-lg bg-red-600 px-4 py-6 transition-colors active:bg-red-700'
-            onPress={() => {
-              Alert.alert(t`Are you sure?`, t`This will reset the NFC data`, [
-                {
-                  text: t`Cancel`,
-                  onPress: () => console.log('Cancel Pressed'),
-                  style: 'cancel',
-                },
-                { text: t`OK`, onPress: () => setUserInput('') },
-              ]);
-            }}>
-            <Text className='text-xl font-semibold'>
-              <Trans>Reset</Trans>
-            </Text>
-          </Pressable>
-        </View>
+    <ScrollView className='flex-1 p-6' contentContainerClassName='gap-4'>
+      <View className='flex-row gap-4'>
+        <Pressable
+          className='grow items-center rounded-lg bg-neutral-800 px-4 py-6 transition-colors active:bg-neutral-700'
+          onPress={writeNdef}>
+          <Text className='text-xl font-semibold'>
+            <Trans>Write NFC Tag</Trans>
+          </Text>
+        </Pressable>
+        <Pressable
+          className='grow items-center rounded-lg bg-red-600 px-4 py-6 transition-colors active:bg-red-700'
+          onPress={() => {
+            Alert.alert(t`Are you sure?`, t`This will reset the NFC data`, [
+              {
+                text: t`Cancel`,
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+              },
+              { text: t`OK`, onPress: () => setUserInput('') },
+            ]);
+          }}>
+          <Text className='text-xl font-semibold'>
+            <Trans>Reset</Trans>
+          </Text>
+        </Pressable>
+      </View>
 
-        <Text className='text-lg font-medium'>
-          <Trans>Input:</Trans>
-        </Text>
-        <View className='rounded-lg bg-neutral-900 p-2'>
-          <TextInput
-            value={userInput}
-            onChangeText={setUserInput}
-            multiline
-            className='text-black dark:text-white'
-          />
-        </View>
-      </ScrollView>
-    </>
+      <Text className='text-lg font-medium'>
+        <Trans>Input:</Trans>
+      </Text>
+      <View className='rounded-lg bg-neutral-900 p-2'>
+        <TextInput
+          value={userInput}
+          onChangeText={setUserInput}
+          multiline
+          className='text-black dark:text-white'
+        />
+      </View>
+    </ScrollView>
   );
 }
